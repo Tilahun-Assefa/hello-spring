80a1cf1d27d14c9bfa6076cd1670193fc493e3e1
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-09-29 19:35:07 -0500

    class annotation done

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,24 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(value="hello")
+@ResponseBody
 public class HelloController {
-    @GetMapping
-    @ResponseBody
-    public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form method='post' action='/hello'>" +
-                "<input type='text' name='coder'/>" +
-                "<input type='submit' value='Greet Me'/>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    //responds to /hello
+    @GetMapping("")
+    public String hello() {
+        return "Hello";
     }
 
-    // responds to get requests at "/hello?coder=LaunchCoder"
-    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String coder) {
-        return "Hello, " + coder + "!";
+    // responds to get /hello/goodbye
+    @GetMapping("goodbye")
+    public String helloGoodbye() {
+        return "Hello, Goodbye";
     }
 }
